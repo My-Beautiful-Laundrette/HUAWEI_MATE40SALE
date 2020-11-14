92be66de662b56170d297a99439e58cd1ef8f84b
zhangshuai <123@163.com>
2020-11-14 10:58:37 +0800

    在"min"上提交

---
+++ src/views/login.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div style="padding-top:16px">
+            <div class="center">
+                 <i class="logo"></i>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+    .center{
+        text-align: center;
+        justify-content: center;
+        -webkit-box-align: center;
+    }
+    .center>i{
+        background:"../assets/login_img/1.png" no-repeat;
+        background-size:100%;
+        background-position:50%;
+    }
+</style>
